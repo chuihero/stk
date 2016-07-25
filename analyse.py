312b0333dcfe74e69647fd692c7dad682a5c42b1
surface <ie.jiahui@outlook.com>
2016-07-25 21:56:56 +0800

    查询固定股票数据

--- analyse.py
+++ analyse.py
@@ -2,9 +2,15 @@
 # coding=utf-8
 
 import pymysql
+import pandas as pd
+from datetime import datetime
+import json
+import sys
 
-class client():
+class Client():
     DATABASE = 'stock'
+    BASICKTABLENAME = 'basic'
+    DAYHISTORYTABLENAME = 'dayHistory'
 
     def __init__(self, sqlConfigFile='sqlConfig.json', char='utf8'):
         try:
@@ -25,3 +31,35 @@ class client():
             sys.exit()
 
         self.cur = self.conn.cursor()
+    def getDayLines(self,code,yearOffset = 3):
+        """
+
+        :param code: 股票代码
+        :param yearOffset: 年份偏执，只需近几年的股票数据
+        :return:
+        """
+        assert type(code)==str, '股票代码应为字符串'
+        self.code = code
+        today = datetime.today()
+        startDate = datetime(today.year - yearOffset,\
+                             today.month,\
+                             today.day)
+
+        s = "select @f:=max(factor) from {} where code = '{}'".format(\
+                                            self.DAYHISTORYTABLENAME,\
+                                            self.code)
+        self.cur.execute(s)
+        s = "select date, open/@f as open, high/@f as high,close/@f as close," \
+            "low/@f as low, volume, amount/@f as amount from {} " \
+            "where code='{}' and date >='{}'".format(self.DAYHISTORYTABLENAME,\
+                                                     self.code,
+                                                     startDate.strftime('%y-%m-%d'))
+        df = pd.read_sql(s,self.conn)
+        return df
+
+
+if __name__=='__main__':
+    c = Client()
+    a = c.getDayLines('000001')
+    print(a)
+
