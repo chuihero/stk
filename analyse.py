2cb1fbf2e5370301508326cc9c6137353c6d8b4d
henry <ie.jiahui@gmail.com>
2016-08-02 21:57:26 +0800

    初步验证分型思路，后续考虑丢掉不满足间隔要求的分型

--- analyse.py
+++ analyse.py
@@ -59,10 +59,112 @@ class Client():
         df = pd.read_sql(s,self.conn)
         return df
 
+class Chan():
+    def __init__(self,code):
+        assert type(code) == str, '股票代码应为字符串'
+        self.code = code
+
+        client = Client()
+        self.df = client.getDayLines(self.code)
+
+    def filterKLines(self):
+        """根据缠论，把k线过滤一遍，删掉包含关系的k线"""
+
+
+        def isInclude(last,next):
+            return last['high']>=next['high'] and last['low']<=next['low']
+
+        def isIncludedBy(last,next):
+            return last['high']<=next['high'] and last['low']>=next['low']
+
+        def popLastReserved():
+            pass;
+
+        def pushLatest():
+            pass;
+        def findAllReserved(x,l):
+            return [a for a in range(len(l)) if l[a]==x]
+
+        filteredRes = [True,]
+        last = self.df.iloc[0]
+
+        for i in range(1,len(self.df)):
+            next = self.df.iloc[i]
+            if isInclude(last,next):
+                filteredRes.append(False)
+                continue
+
+            elif isIncludedBy(last,next):
+                # 需要回退，直到不再被包含
+                truePosition = findAllReserved(True,filteredRes)
+                while len(truePosition)>=1:
+                    p = truePosition.pop()
+                    filteredRes[p] = False
+                    last = self.df.iloc[truePosition[-1]]
+                    if isIncludedBy(last,next):
+                        if len(truePosition) == 0:
+                            #回退到第一个
+                            last = next
+                            filteredRes.append(True)
+                            break
+
+                        continue
+                    elif isInclude(last,next):
+                        filteredRes.append(False)
+                    else:
+                        filteredRes.append(True)
+                        last = next
+                    break
+                continue
+            else:
+                filteredRes.append(True)
+                last = next
+                continue
+
+        self.df.loc[:,'reserved'] = filteredRes
+        return self
+
+    def findPeakBottom(self,gap=4):
+        """
+        找到k线中的高点和低点
+        :param :
+            gap:顶分和底分之间的间隔，默认为4
+        :return:
+        """
+
+        def isRise(next,last):
+            return next['high']>=last['high']
+
+        def isPeak(next,last,status):
+            return (not isRise(next,last)) and status =='rise'
+
+        def isBottom(next,last,status):
+            return (isRise(next,last)) and status == 'fall'
+
+        res = []
+        filteredDf = self.df[self.df['reserved']==True]
+
+        last = filteredDf.iloc[0]
+        status = ''
+        for i in range(1,len(filteredDf)):
+            next = filteredDf.iloc[i]
+            if isPeak(next,last,status):
+                #stock rise
+                res.append('peak')
+            elif isBottom(next,last,status):
+                res.append('bottom')
+            else:
+                #普通上涨或下跌，但不是顶分或底分
+                res.append('rise' if isRise(next,last) else 'fall')
+
+            status = 'rise' if isRise(next,last) else 'fall'
+            last = next
+            continue
+        filteredDf.loc[1:,'fenxing'] = res
+        print(filteredDf)
 
 if __name__=='__main__':
-    c = Client()
-    a = c.getDayLines('000001')
-
-    print(a)
+    c = Chan('000001')
+    c.filterKLines()
+    c.findPeakBottom()
 
